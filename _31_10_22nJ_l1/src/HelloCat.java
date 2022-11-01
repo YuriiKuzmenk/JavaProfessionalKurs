f3b3c2cd4f6725ba75fa61da487c47af6f3b9c60
Yurii Kuzmenko <108761481+YuriiKuzmenk@users.noreply.github.com>
2022-11-01 16:06:53 +0100

    new new

--- _31_10_22nJ_l1/src/HelloCat.java
+++
@@ -1,8 +0,0 @@
-public class HelloCat {
-    public static void main(String[] args) {
-        Cat cat = new Cat("Barsik", "red",4 );
-
-        cat.voice();
-        System.out.println(cat);
-    }
-}
