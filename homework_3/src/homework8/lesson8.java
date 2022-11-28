92c2abc0882e499bb09b2472bc9c3a4307d0da6e
Yurii Kuzmenko <108761481+YuriiKuzmenk@users.noreply.github.com>
2022-11-28 17:32:51 +0100

    hw8

---
+++ homework_3/src/homework8/lesson8.java
@@ -0,0 +1,24 @@
+package homework8;
+
+/**
+ * Java. Homework №7.
+ *
+ * @author Yurii Kuzmenko java_39e
+ * @version 24.11.2022 - 28.11.2022
+ */
+
+public class lesson8 {
+    public static void main(String[] args) {
+        RubberList<Integer> rl = new RubberList<>();
+        System.out.println(rl);
+        rl.add(12);
+        //rl.add(4);
+       // rl.add(8);
+        rl.add(3);
+        System.out.println(rl);
+        rl.remove(12);
+       // rl.remove(8);
+      //  rl.remove(3);
+        System.out.println(rl);
+    }
+}
---
+++ homework_3/src/lesson8/lesson8.java
@@ -0,0 +1,17 @@
+package lesson8;
+
+public class lesson8 {
+    public static void main(String[] args) {
+        RubberList <Integer> rl = new RubberList<>();
+        System.out.println(rl);
+        rl.add(12);
+        rl.add(4);
+        rl.add(8);
+        rl.add(3);
+        System.out.println(rl);
+        rl.remove(12);
+        rl.remove(8);
+        rl.remove(3);
+        System.out.println(rl);
+    }
+}
